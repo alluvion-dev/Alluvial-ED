b828b8855026ad2fa4a63ab792daa8b18d0efdee
alluvion-dev <111410825+alluvion-dev@users.noreply.github.com>
2022-08-18 13:55:54 +0900

    Install static adapter
    isn't working currently though

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+// import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
@@ -13,3 +14,23 @@ const config = {
 };
 
 export default config;
+
+// export default {
+// 	kit: {
+// 	  adapter: adapter({
+// 		// default options are shown. On some platforms
+// 		// these options are set automatically — see below
+// 		pages: 'build',
+// 		assets: 'build',
+// 		fallback: null,
+// 		precompress: false
+// 	  }),
+  
+// 	  prerender: {
+// 		// This can be false if you're using a fallback (i.e. SPA mode)
+// 		default: true
+// 	  },
+
+// 	  trailingSlash: 'always'
+// 	}
+// };
